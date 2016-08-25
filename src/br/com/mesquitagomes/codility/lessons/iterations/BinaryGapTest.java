6e92bdb2648975364be3f0a8294d89e062357780
M4rc3l <mesquitagomes@gmail.com>
2016-08-25 11:45:05 -0300

    Rename BinaryGapJUTest to BinaryGapTest

--- src/br/com/mesquitagomes/codility/lessons/iterations/BinaryGapTest.java
+++ src/br/com/mesquitagomes/codility/lessons/iterations/BinaryGapTest.java
@@ -6,7 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class BinaryGapJUTest extends TestCase {
+public class BinaryGapTest extends TestCase {
 
 	private BinaryGap bg;
 	private int[] nArray = null;
