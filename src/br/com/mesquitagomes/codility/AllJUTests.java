22959c4aa5c4ad77206daded34183fc987262b2b
M4rc3l <mesquitagomes@gmail.com>
2016-08-25 11:46:11 -0300

    Update AllJUTests.java
    Rename BinaryGapJUTest to BinaryGapTest

--- src/br/com/mesquitagomes/codility/AllJUTests.java
+++ src/br/com/mesquitagomes/codility/AllJUTests.java
@@ -4,10 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import br.com.mesquitagomes.codility.lessons.iterations.BinaryGapJUTest;
+import br.com.mesquitagomes.codility.lessons.iterations.BinaryGapTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BinaryGapJUTest.class })
+@SuiteClasses({ BinaryGapTest.class })
 public class AllJUTests {
 
 }
